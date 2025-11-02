898ab076e75a0d5f3965df2cdbb5b0e050328fbc
LEE SEEUN <thjds8@gmail.com>
2025-11-02 22:05:04 +0900

    add button (#1)
    Co-authored-by: [이세은][기술개발본부][대리] <128339022+seeun1025@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
       <header className="App-header">
         <h1>Fridge Freezer</h1>
         <p>냉장고 관리 앱에 오신 것을 환영합니다!</p>
+        <button onClick={() => {
+          console.log('Add Item');
+        }}>Add Item</button>
       </header>
     </div>
   );
