025853dcbdbf2b553a81e79be30d28afa50f7925
[이세은][기술개발본부][대리] <128339022+seeun1025@users.noreply.github.com>
2025-11-02 21:51:31 +0900

    make react app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Fridge Freezer</h1>
+        <p>냉장고 관리 앱에 오신 것을 환영합니다!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
